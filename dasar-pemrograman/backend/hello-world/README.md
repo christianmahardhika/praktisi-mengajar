52e25604c4b0afa5fd46dec5a5daeffaa1201059
Christian Mahardhika <35090186+christianmahardhika@users.noreply.github.com>
2023-11-07 10:36:46 +0700

    add documentation

---
+++ dasar-pemrograman/backend/hello-world/README.md
@@ -0,0 +1,11 @@
+# FILEPATH: /hello-world
+"""
+This project folder contains a simple "Hello, World!" program. 
+
+## How to run
+
+```bash
+node index.js
+```
+
+"""
--- dasar-pemrograman/backend/hello-world/index.js
+++ dasar-pemrograman/backend/hello-world/index.js
@@ -1,15 +1,23 @@
+// Import the express module
 const express = require('express');
+
+// Create a new express application
 const app = express();
 
+// Define a route handler for the default home page
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// Define a route handler for the /json path to return some JSON data
 app.get('/json', (req, res) => {
   res.json({
     message: 'Hello World!',
   });
 });
 
+// Start the server on port 3000
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!', 'http://localhost:3000');
+  console.log('Server started on port 3000');
 });
+const express = require('express');
